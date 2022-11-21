52e1e12acf201b67d9f5914016f5e915cb483e14
XaaRii <pgambler@seznam.cz>
2022-11-21 22:33:09 +0100

    there is a limiter, huh

--- commands/prime.js
+++ commands/prime.js
@@ -19,7 +19,8 @@ module.exports = {
 			// [ bundle name, game name, time, url ]
 			if (e[0] !== "Undetectable" && e[1] === "Undetectable") e[1] = "Free to claim game";
 			if (e[3] === "Link Undetectable") {
-				embed.addFields({ name: e[0], value: `${e[1]}\nExpires in${e[2]})`, inline: false });
+				// just skip this for now
+				// embed.addFields({ name: e[0], value: `${e[1]}\nExpires in${e[2]})`, inline: false });
 			} else embed.addFields({ name: e[0], value: `${e[1]}\nExpires in${e[2]}\n[offer link](${e[3]})`, inline: false });
 		}
 		embed.setTitle("Current Twitch Prime offers:")
