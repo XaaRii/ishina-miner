9154879e30b5f4d952373ada32a827775a41cc54
XaaRii <pgambler@seznam.cz>
2023-02-20 21:07:12 +0100

    fix stdout + stderr

--- twitchminers/starter.sh
+++ twitchminers/starter.sh
@@ -7,9 +7,8 @@ fi
 filename="run"$1".py"
 tminer="tm-"$1
 echo "$filename"
-python $filename 2> ./templogs/$tminer.err || (
+python $filename 2>&1 | tee >(grep -A 1000 "Traceback" > ./templogs/$tminer.err) > /dev/tty || (
 	echo "error detected"
-	err=$(cat ./templogs/$tminer.err)
 	curl -i -H "Accept: application/json" -H "Content-Type:application/json" -X POST --data "{\"content\":\"TMERR$1\"}" "https://discord.com/api/webhooks/1028704305789808753/IJgd3TDiTe-I7latCKsR_0BvQZwyaqeo3HOF2o6v4C0jC6wrRN1g0qqLkEy1shbYqn1i" >/dev/null
 	echo "Error webhook sent."
 )
