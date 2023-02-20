76ae98fb449b3d3ebdb830a1bc0217953e7031e5
XaaRii <pgambler@seznam.cz>
2023-02-20 19:51:04 +0100

    starter.sh for error handle

---
+++ twitchminers/starter.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+if [ "$1" == "" ]
+then
+	echo No author ID provided, exiting...
+	exit 1
+fi
+filename="run"$1".py"
+tminer="tm-"$1
+echo "$filename"
+python $filename 2> ./templogs/$tminer.err || (
+	echo "error detected"
+	err=$(cat ./templogs/$tminer.err)
+	curl -i -H "Accept: application/json" -H "Content-Type:application/json" -X POST --data "{\"content\":\"TMERR$1\"}" "https://discord.com/api/webhooks/1028704305789808753/IJgd3TDiTe-I7latCKsR_0BvQZwyaqeo3HOF2o6v4C0jC6wrRN1g0qqLkEy1shbYqn1i" >/dev/null
+	echo "Error webhook sent."
+)
