a57a93be843ad85f480b76081ece2259276bdffe
XaaRii <pgambler@seznam.cz>
2023-02-20 22:45:48 +0100

    added traceback handle

--- twitchminers/runDefault.py
+++ twitchminers/runDefault.py
@@ -9,53 +9,56 @@ from TwitchChannelPointsMiner.classes.Telegram import Telegram
 from TwitchChannelPointsMiner.classes.Settings import Priority, Events, FollowersOrder
 from TwitchChannelPointsMiner.classes.entities.Bet import Strategy, BetSettings, Condition, OutcomeKeys, FilterCondition, DelayMode
 from TwitchChannelPointsMiner.classes.entities.Streamer import Streamer, StreamerSettings
+import traceback
+import sys
 
-twitch_miner = TwitchChannelPointsMiner(
-    username="usernamegoeshere",
-    password="",
-    claim_drops_startup=True,
-    priority=[
-        Priority.DROPS,
-        Priority.STREAK,
-        Priority.ORDER
-    ],
-    enable_analytics=False,
-    logger_settings=LoggerSettings(
-        save=False,
-        console_level=logging.INFO,
-        console_username=False,
-        file_level=logging.DEBUG,
-        emoji=False,
-        less=True,
-        colored=True,
-        color_palette=ColorPalette(
-            STREAMER_online="GREEN",
-            streamer_offline="red",
-            BET_wiN=Fore.MAGENTA
-        )
-    ),
-    streamer_settings=StreamerSettings(
-        make_predictions=False,
-        follow_raid=True,
-        claim_drops=True,
-        watch_streak=True,
-        chat=ChatPresence.ONLINE,
-        bet=BetSettings(
-            strategy=Strategy.SMART,
-            percentage=5,
-            percentage_gap=20,
-            max_points=50000,
-            stealth_mode=True,
-            delay_mode=DelayMode.FROM_END,
-            delay=6,
-            minimum_points=20000,
-            filter_condition=FilterCondition(
-                by=OutcomeKeys.TOTAL_USERS,
-                where=Condition.LTE,
-                value=800
+try:
+    twitch_miner = TwitchChannelPointsMiner(
+        username="usernamegoeshere",
+        password="",
+        claim_drops_startup=True,
+        priority=[
+            Priority.DROPS,
+            Priority.STREAK,
+            Priority.ORDER
+        ],
+        enable_analytics=False,
+        logger_settings=LoggerSettings(
+            save=False,
+            console_level=logging.INFO,
+            console_username=False,
+            file_level=logging.DEBUG,
+            emoji=False,
+            less=True,
+            colored=True,
+            color_palette=ColorPalette(
+                STREAMER_online="GREEN",
+                streamer_offline="red",
+                BET_wiN=Fore.MAGENTA
+            )
+        ),
+        streamer_settings=StreamerSettings(
+            make_predictions=False,
+            follow_raid=True,
+            claim_drops=True,
+            watch_streak=True,
+            chat=ChatPresence.ONLINE,
+            bet=BetSettings(
+                strategy=Strategy.SMART,
+                percentage=5,
+                percentage_gap=20,
+                max_points=50000,
+                stealth_mode=True,
+                delay_mode=DelayMode.FROM_END,
+                delay=6,
+                minimum_points=20000,
+                filter_condition=FilterCondition(
+                    by=OutcomeKeys.TOTAL_USERS,
+                    where=Condition.LTE,
+                    value=800
+                )
             )
         )
     )
-)
 
-twitch_miner.mine()
+    twitch_miner.mine()
