ada040a90bbc03f4ec3bd3e845e38ac0fa9bb6dd
Ishina Miner <ishina-miner@alkanixor.com>
2023-03-03 09:30:54 +0100

    fix broken primecheck

--- commands/view.js
+++ commands/view.js
@@ -10,6 +10,7 @@ module.exports = {
 	aliases: ["read", "open"],
 	showHelp: true,
 	async execute(message, args) {
+		const embed = new EmbedBuilder();
 		const authorid = (args[0] && message.author.id === config.xaari) ? args[0] : message.author.id;
 
 		tmmachines.find({ tmowner: authorid }, async function (err, docs) {
@@ -19,21 +20,24 @@ module.exports = {
 			await spectator(0, viewMsg);
 
 			async function spectator(i, viewMsg) {
-	    	const embed = new EmbedBuilder();
 				exec(`screen -S tm-${authorid} -X hardcopy "./templogs/${authorid}.log"`, async function (e, o, oe) {
 					if (e) {
 						// process stopped
-						if (i < 6) embed.setDescription("Twitch miner is not running.\nNext refresh in 10 seconds. (" + i + "/6)");
+						if (i < 5) embed.setDescription("Twitch miner is not running.\nNext refresh in 10 seconds. (" + i + "/5)");
 						else embed.setDescription("Twitch miner is not running.");
 						embed.setColor('e82e2e')
 							.setTitle(docs[0].tmusername + "'s miner")
 							.setTimestamp()
 							.setFooter({ text: `Need help? type ${prefix}help (command)!` });
+						if (i < 1) viewMsg = await message.channel.send({ embeds: [embed] }).catch(er => { message.reply({ content: "something fucked up, " + er }); });
+							else viewMsg.edit({ embeds: [embed] }).catch(er => { message.reply({ content: "something fucked up, " + er }); });
+							if (i < 5) { setTimeout(() => { return spectator((i + 1), viewMsg); }, 10000); }
+							else return;
 					} else {
 						exec(`screen -S tm-${authorid} -X hardcopy "./templogs/${authorid}.log" && sleep 1 && tac ./twitchminers/templogs/${authorid}.log | grep -m 10 '[[:blank:]]' | tac`, async function (ee, oo, ooee) {
 							if (ee) return message.reply("Something fucked up, please report to Pawele.");
-							if (i < 6) embed.setDescription("Next refresh in 10 seconds. (" + i + "/6)");
-							else embed.setDescription("Closed view (" + i + "/6)");
+							if (i < 5) embed.setDescription("Next refresh in 10 seconds. (" + i + "/5)");
+							else embed.setDescription("Closed view (" + i + "/5)");
 							embed.setColor('43ea46')
 								.setTitle(docs[0].tmusername + "'s miner")
 								.setFields([
@@ -43,15 +47,12 @@ module.exports = {
 								])
 								.setTimestamp()
 								.setFooter({ text: `Need help? type ${prefix}help (command)!` });
+							if (i < 1) viewMsg = await message.channel.send({ embeds: [embed] }).catch(er => { message.reply({ content: "something fucked up, " + er }); });
+							else viewMsg.edit({ embeds: [embed] }).catch(er => { message.reply({ content: "something fucked up, " + er }); });
+							if (i < 5) { setTimeout(() => { return spectator((i + 1), viewMsg); }, 10000); }
+							else return;
 						});
 					}
-					if (i < 1) viewMsg = await message.channel.send({ embeds: [embed] }).catch(er => { message.reply({ content: "something fucked up, " + er }); });
-					else viewMsg.edit({ embeds: [embed] }).catch(er => { message.reply({ content: "something fucked up, " + er }); });
-					if (i < 6) {
-						setTimeout(() => {
-							return spectator((i + 1), viewMsg);
-						}, 5000);
-					} else return;
 				});
 			}
 		});
--- primecheck/primecheck.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-mv prime_fetch.json prime_fetch_old.json
-python prime_checker.py
