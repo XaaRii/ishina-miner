4ce0221fc2c5373a9fafb64016f087f3dde8648d
XaaRii <pgambler@seznam.cz>
2023-03-12 21:57:54 +0100

    corrected post request form

--- ishina.js
+++ ishina.js
@@ -140,7 +140,7 @@ client.on(Events.MessageCreate, async message => {
 					for (const file of slashFiles) {
 						const command = require(`./slashcmds/${file}`);
 						client.slashCollection.set(command.data.name, command.data.toJSON());
-						args[1] === "overwrite" ? slashCommands.push(command) : await rest.post(Routes.applicationCommands(config.dcAppID, message.guildId), { body: command });
+						args[1] === "overwrite" ? slashCommands.push(command) : await rest.post(Routes.applicationCommands(config.dcAppID, message.guildId), { body: command.data.toJSON() });
 						i++;
 					}
 					console.log(`deploy of ${i} slash commands globally on ${message.author.username}'s request.`);
@@ -160,7 +160,7 @@ client.on(Events.MessageCreate, async message => {
 						const command = require(`./slashcmds/${file}`);
 						client.slashCollection.set(command.data.name, command.data.toJSON());
 						if (!command.developer) {
-							args[1] === "overwrite" ? slashPubCommands.push(command) : await rest.post(Routes.applicationCommands(config.dcAppID), { body: command });
+							args[1] === "overwrite" ? slashPubCommands.push(command) : await rest.post(Routes.applicationCommands(config.dcAppID), { body: command.data.toJSON() });
 							i++;
 						}
 					}
