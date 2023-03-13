1427844f734a8ab052852c3b7b450a8ed5a6c974
XaaRii <pgambler@seznam.cz>
2023-03-13 17:52:12 +0100

    added slash reloading?

---
+++ commands/reloadslash.js
@@ -0,0 +1,28 @@
+const config = require("../.cfg.json");
+
+module.exports = {
+	name: 'reloadslash',
+	description: 'Reloads a slash command',
+	aliases: ['rls'],
+	usage: '<command you want to reload>',
+	showHelp: false,
+	execute(message, args) {
+		if (message.author.id !== config.xaari) return;
+		message.channel.sendTyping();
+		if (!args.length) return message.reply("I don't know what command you wanna reload!");
+		const commandName = args[0].toLowerCase();
+		const command = message.client.slashCollection.get(commandName);
+		if (!command) return message.channel.send(`That's not a command I would know, ${message.author}!`);
+
+		delete require.cache[require.resolve(`./${command.name}.js`)];
+
+		try {
+			const newCommand = require(`./${command.name}.js`);
+			message.client.slashCollection.set(newCommand.name, newCommand);
+			message.channel.send(`Command \`${command.name}\` reloaded!`);
+		} catch (error) {
+			console.log(error);
+			message.channel.send(`I've got an error while reloading that \`${command.name}\` command:\n\`${error.message}\``);
+		}
+	},
+};
