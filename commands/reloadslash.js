a9c98cabe6dab1e412cdbc398802a82a8171051e
XaaRii <pgambler@seznam.cz>
2023-03-15 14:59:34 +0100

    rls better now

--- commands/reloadslash.js
+++ commands/reloadslash.js
@@ -1,4 +1,7 @@
 const config = require("../.cfg.json");
+var token = config.dcToken, appid = config.dcAppID;
+const { REST, Routes } = require("discord.js");
+const rest = new REST({ version: '10' }).setToken(token);
 
 module.exports = {
 	name: 'reloadslash',
@@ -6,19 +9,22 @@ module.exports = {
 	aliases: ['rls'],
 	usage: '<command you want to reload>',
 	showHelp: false,
-	execute(message, args) {
+	async execute(message, args) {
 		if (message.author.id !== config.xaari) return;
-		message.channel.sendTyping();
 		if (!args.length) return message.reply("I don't know what command you wanna reload!");
+		if (!["g", "l", "n"].some(v => args[1].includes(v))) return message.reply("g/l/n ?");
+		message.channel.sendTyping();
 		const commandName = args[0].toLowerCase();
 		const command = message.client.slashCollection.get(commandName);
 		if (!command) return message.channel.send(`That's not a command I would know, ${message.author}!`);
 
-		delete require.cache[require.resolve(`../slashcmds/${commandName}.js`)];
+		await delete require.cache[require.resolve(`../slashcmds/${commandName}.js`)];
 
 		try {
 			const newCommand = require(`../slashcmds/${commandName}.js`);
 			message.client.slashCollection.set(newCommand.data.name, newCommand);
+			if (args[1] === "g") await rest.post(Routes.applicationCommands(appid), { body: command.data.toJSON() });
+			if (args[1] === "l") await rest.post(Routes.applicationCommands(appid, message.guildId), { body: command.data.toJSON() });
 			message.channel.send(`Command \`${command.data.name}\` reloaded!`);
 		} catch (error) {
 			console.log(error);
