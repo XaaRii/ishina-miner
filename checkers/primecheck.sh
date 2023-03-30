ea0911ff686ab1eb4100dd26896ee4d538570b29
XaaRii <pgambler@seznam.cz>
2023-03-30 21:48:50 +0200

    minor fix for nonworking primechecker

--- checkers/primecheck.sh
+++ checkers/primecheck.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
 mv prime_fetch.json prime_fetch_old.json
-python prime_checker.py
+python prime_checker.py || (
+	echo "error detected"
+	cp prime_fetch_old.json prime_fetch.json
+	webhook=$(cat "../.cfg.json" | jq -r '.errWebhook')
+	curl -i -H "Accept: application/json" -H "Content-Type:application/json" -X POST --data "{\"content\":\"PRIME-ERR\"}" "$webhook" >/dev/null
+	echo "Error webhook sent."
+)
