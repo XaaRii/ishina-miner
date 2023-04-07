109f17770c91678068d7120affaf652923bec1c8
XaaRii <pgambler@seznam.cz>
2023-04-07 21:12:00 +0200

    progressbar fix

--- ishina.js
+++ ishina.js
@@ -134,9 +134,10 @@ client.on(Events.MessageCreate, async message => {
 			} else return message.reply("**ᴀᴄᴄᴇꜱꜱ ᴅᴇɴɪᴇᴅ**, get lost.");
 		case "deploy":
 			if (message.author.id !== config.xaari) return;
+			if (!["global", "local"].includes(args[0])) message.channel.send("Missing argument: local/global (overwrite)");
 			message.channel.sendTyping();
 			var resp = ['Registering commands in progress...\n'];
-			var progressbar = args[1] !== "overwrite" ? message.reply({ content: resp.join("") }) : undefined;
+			var progressbar = args[1] !== "overwrite" ? await message.reply({ content: resp.join("") }) : undefined;
 			if (args[0] === "local") {
 				try {
 					const slashCommands = [];
